e28e578cd29b3e00abe087c6789c24e93271cc17
tom842422 <tom842422@163.com>
2022-07-25 22:28:06 +0800

    增加了查询的接口和测试数据

--- sosoyy_3_api/untils/random_data.py
+++ sosoyy_3_api/untils/random_data.py
@@ -2,14 +2,14 @@ import string
 import random
 
 
-def uncode():
-    return ''.join(random.sample(string.ascii_letters + string.digits, 8))
-
-
-def number():
-    return ''.join(random.sample(string.ascii_letters + string.digits, 8))
-
-
-print(uniquecode())
-print(pwnumber())
-# print(salt)
+# def uncode():
+#     return ''.join(random.sample(string.ascii_letters + string.digits, 8))
+#
+#
+# def number():
+#     return ''.join(random.sample(string.ascii_letters + string.digits, 8))
+#
+#
+# print(uniquecode())
+# print(pwnumber())
+# # print(salt)
